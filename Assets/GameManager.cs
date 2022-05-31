b7551644c15862104d2bb99a417c6467d9637326
ReoMiyazaki <k021g1153@g.neec.ac.jp>
2022-06-01 00:04:20 +0900

    KorokoroBall完成

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;  // using‚ð–Y‚ê‚È‚¢‚Å
 
 public class GameManager : MonoBehaviour
 {
-  public void SceneReset()
+    public void SceneReset()
     {
         string activeSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(activeSceneName);
     }
+
+    public void ChangeScene(string nextScene)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    private void Start()
+    {
+        Screen.SetResolution(1920, 1080, false);
+        Application.targetFrameRate = 60;
+    }
+
 }
